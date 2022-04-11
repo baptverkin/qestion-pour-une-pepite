95eb15759be3da32a43a2c75f477831ecfd95590
Baptiste Verkindere <bverkindere@gmail.com>
2022-04-11 14:55:57 +0200

    landing page with nabar and footer in Layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,14 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
+import { Layout } from '../components/layout'
 import styles from '../styles/Home.module.css'
+
 
 const Home: NextPage = () => {
   return (
+    <Layout >
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
@@ -13,59 +17,46 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
+        <h3 className={styles.title}>
+          Bienvenue sur la plateforme officielle de <br></br>Question pour une Pépite
+        </h3><br></br><br></br>
+      <Link href="/profile" passHref={true}>
+        <button type="button" className="btn btn-primary" >
+          Commencer une partie &rarr;
+        </button>
+        </Link><br></br><br></br>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          <p className={styles.card}>
+            <h2>9 points gagnants</h2>
+            <p>Le dernier à 9 points perd la manche</p>
+          </p>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          <a className={styles.card}>
+            <h2>4 à la suite</h2>
+            <p>Répondez correctement à 4 question successives </p>
           </a>
 
           <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
           >
-            <h2>Deploy &rarr;</h2>
+            <h2>Face à Face</h2>
+            <p>Duel au sommet entre les finalistes, le premier à 12 points gagne la partie</p>
+          </a>
+
+          <a
+            href="/regles"
+            className={styles.card}
+          >
+            <h2>Règles &rarr;</h2>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              Faire un point sur les règles du jeu
             </p>
           </a>
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
+    </Layout>
   )
 }
 
