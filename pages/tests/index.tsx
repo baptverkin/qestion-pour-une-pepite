218c837b9f576d0c8634a82edb5ea1e254d469ff
Lucas Gillon <lucas.gillon98@gmail.com>
2022-04-11 17:36:57 +0200

    pb auth0

---
+++ pages/tests/index.tsx
@@ -0,0 +1,22 @@
+import { useUser } from "@auth0/nextjs-auth0";
+import Link from "next/link";
+import { Layout } from "../../components/layout";
+
+const Cart: React.FC = () => {
+  const { user } = useUser();
+  if (user) {
+    return (
+      <Layout>
+        <div className="container">{user.name}</div>
+      </Layout>
+    );
+  } else {
+    return (
+      <Layout>
+        <div className="container">TEST</div>
+      </Layout>
+    );
+  }
+};
+
+export default Cart;
