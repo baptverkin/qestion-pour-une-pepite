a7d182674f437e9d1d9ebbc6accdfe6fb28dda78
Lucas Gillon <lucas.gillon98@gmail.com>
2022-04-12 13:43:17 +0200

    update user post form et redirection vers l'accueil + affichage du pseudo sur l'index si l'user est connecté

--- pages/form.tsx
+++ pages/form.tsx
@@ -1,6 +1,7 @@
 import { useUser } from "@auth0/nextjs-auth0";
 import { GetServerSideProps } from "next";
-import Link from "next/link";
+import router from "next/router";
+import React from "react";
 import { Layout } from "../components/layout";
 import { getDatabase } from "../src/database";
 
@@ -9,7 +10,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
 
   const response = await mongodb.db().collection("users").find().toArray();
   const users = await JSON.parse(JSON.stringify(response));
-  // console.log(users);
 
   return {
     props: {
@@ -20,37 +20,49 @@ export const getServerSideProps: GetServerSideProps = async () => {
 
 const Form: React.FC<{ email: string }> = ({ email }) => {
   const { user } = useUser();
+  const [input, setInput] = React.useState();
+  const handleChange = (e: { preventDefault: () => void; target: any }) => {
+    e.preventDefault();
+    const target = e.target;
+    setInput(target.value);
+  };
+  const handleSubmit = (e: { preventDefault: () => void; target: any }) => {
+    e.preventDefault();
+    const temp = {
+      pseudo: input,
+      email: email,
+    };
+    fetch("/api/update_user", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(temp),
+    });
+    router.push("/");
+  };
   return (
     <Layout>
       {user ? (
         <div className="container">
-          Email: {email}
-          <div className="mb-3">
-            <label htmlFor="exampleFormControlInput1" className="form-label">
-              Choisissez un pseudo :
-            </label>
-            <input
-              type="email"
-              className="form-control"
-              id="exampleFormControlInput1"
-              placeholder="pseudo"
-            />
-            <button>
-              <Link href={""}></Link>
-            </button>
-          </div>
+          <form>
+            Email: {email}
+            <div className="mb-3">
+              <label htmlFor="exampleFormControlInput1" className="form-label">
+                Choisissez un pseudo :
+              </label>
+              <input type="text" placeholder="pseudo" onChange={handleChange} />
+              <button type="submit" onClick={handleSubmit}>
+                Submit
+              </button>
+            </div>
+          </form>
         </div>
       ) : (
         ""
       )}
     </Layout>
   );
-
-  // if (user) {
-  //   return <div className="container">test {email}</div>;
-  // } else {
-  //   return <div className="container">TEST 2</div>;
-  // }
 };
 
 export default Form;
