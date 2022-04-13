38a85730843064893ed84aba224b6bf9d494bcf4
Yohan M <inuyasha59260@gmail.com>
2022-04-13 07:54:38 +0200

    BasicFront

---
+++ pages/Game1.tsx
@@ -0,0 +1,73 @@
+import { useUser } from "@auth0/nextjs-auth0";
+import type { GetServerSideProps, NextPage } from "next";
+import Link from "next/link";
+import { Layout } from "../components/layout";
+import styles from "../styles/Home.module.css";
+import { getDatabase } from "../src/database";
+import { getSession } from "@auth0/nextjs-auth0";
+import { CountdownCircleTimer } from
+	'react-countdown-circle-timer'
+
+
+
+
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  const session = getSession(req, res);
+  const email = session?.user.email;
+
+  const mongodb = await getDatabase();
+  const response = await mongodb
+    .db()
+    .collection("users")
+    .findOne({ email: email });
+  const userDB = JSON.parse(JSON.stringify(response));
+
+  return {
+    props: {
+      userDB: userDB,
+    },
+  };
+};
+
+const Game1: React.FC<{ userDB: any }> = ({ userDB }) => {
+  return(
+    <Layout>
+    <div className={styles.title} style= {{marginTop: "20px"}}> 9 points gagnants</div>
+    <div className={styles.description}> Question: quelle est la couleur de la mer noire?</div>
+<div><CountdownCircleTimer className={styles.title}
+		isPlaying
+		duration={30}
+		colors={[
+		["#004777", 0.33],
+		["#F7B801", 0.33],
+		['#A30000', 0.33],
+		]}
+	>
+		{({ remainingTime }) => remainingTime}
+	</CountdownCircleTimer></div>
+<div className="container">
+  <div className="row">
+  <div className="column"   > <button >1</button></div>
+  <div className="column"><button>2</button></div>
+
+  <div className="row">
+  <div className="column"   > <button >3</button></div>
+  <div className="column"><button>4</button></div>
+</div>
+
+<div className="row">
+  <div className="column"   > <button >5</button></div>
+  <div className="column"><button>6</button></div>
+</div>
+</div>
+</div>
+
+
+    </Layout>
+  )
+}
+
+
+
+export default Game1
