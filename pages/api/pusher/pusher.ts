9265124bd2633bc8126506224d058d60cd6b217d
Lucas Gillon <lucas.gillon98@gmail.com>
2022-04-19 09:28:41 +0200

    pusher js to ts

---
+++ pages/api/pusher/pusher.ts
@@ -0,0 +1,24 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import Pusher from "pusher";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const APP_ID = process.env.APP_ID || "";
+  const APP_KEY = process.env.APP_KEY || "";
+  const APP_SECRET = process.env.APP_SECRET || "";
+  const APP_CLUSTER = process.env.APP_CLUSTER || "";
+  const pusher = new Pusher({
+    appId: APP_ID,
+    key: APP_KEY,
+    secret: APP_SECRET,
+    cluster: APP_CLUSTER,
+  });
+
+  pusher.trigger("tests", "test-event", {
+    name: "Hugo",
+    score: Math.random(),
+    messageType: "Join 9 points gagnants",
+  });
+  res.end();
+};
+
+export default handler;
