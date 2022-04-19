0543e0137e48bb0fc7ac21cb191d4e3db732115c
Lucas Gillon <lucas.gillon98@gmail.com>
2022-04-20 01:26:46 +0200

    css + optimisation app.tsx in ssrprovider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,15 @@ import "../styles/globals.css";
 import "bootstrap/dist/css/bootstrap.css";
 import type { AppProps } from "next/app";
 import { UserProvider } from "@auth0/nextjs-auth0";
+import { SSRProvider } from "react-bootstrap";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <UserProvider>
-      <Component {...pageProps} />
-    </UserProvider>
+    <SSRProvider>
+      <UserProvider>
+        <Component {...pageProps} />
+      </UserProvider>
+    </SSRProvider>
   );
 }
 
