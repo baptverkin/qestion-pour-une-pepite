3f492b9e84ab7b2ba17617b799c72b409220d5fc
Yohan M <inuyasha59260@gmail.com>
2022-04-20 08:45:09 +0200

    CSSPageAccueuil

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import { Layout } from "../components/layout";
 import styles from "../styles/Home.module.css";
 import { getDatabase } from "../src/database";
 import { getSession } from "@auth0/nextjs-auth0";
+import Image from "next/image";
 
 export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
   const session = getSession(req, res);
@@ -30,8 +31,7 @@ const Home: React.FC<{ userDB: any; session: any }> = ({ userDB, session }) => {
       <div className={styles.container}>
         <main className={styles.main}>
           <h3 className={styles.title}>
-            Bienvenue sur la plateforme officielle de <br />
-            Question pour une Pépite
+<Image src={"/images/logo_qpup_transparent.png"} alt= "logo" width="600px" height="350px" ></Image>
           </h3>
           <p>
             {userDB ? `Joueur actuellement connecté : ${userDB.pseudo}` : ""}
@@ -61,30 +61,6 @@ const Home: React.FC<{ userDB: any; session: any }> = ({ userDB, session }) => {
           <br />
           <br />
 
-          <div className={styles.grid}>
-            <a className={styles.card}>
-              <h2>9 points gagnants</h2>
-              <p>Le dernier à 9 points perd la manche</p>
-            </a>
-
-            <a className={styles.card}>
-              <h2>4 à la suite</h2>
-              <p>Répondez correctement à 4 question successives </p>
-            </a>
-
-            <a className={styles.card}>
-              <h2>Face à Face</h2>
-              <p>
-                Duel au sommet entre les finalistes, le premier à 12 points
-                gagne la partie
-              </p>
-            </a>
-
-            <a href="/regles" className={styles.card}>
-              <h2>Règles &rarr;</h2>
-              <p>Faire un point sur les règles du jeu</p>
-            </a>
-          </div>
         </main>
       </div>
     </Layout>
